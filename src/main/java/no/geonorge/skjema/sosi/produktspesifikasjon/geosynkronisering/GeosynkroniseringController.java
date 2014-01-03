0a7d9c398303eb176b120c449f95c6029c24ebbc
Malte Stratmann <mst@skogoglandskap.no>
2014-01-03 12:06:18 +0100

    lagt til testfunksjoner til GUI

--- src/main/java/no/geonorge/skjema/sosi/produktspesifikasjon/geosynkronisering/GeosynkroniseringController.java
+++ src/main/java/no/geonorge/skjema/sosi/produktspesifikasjon/geosynkronisering/GeosynkroniseringController.java
@@ -10,6 +10,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import no.geonorge.skjema.sosi.produktspesifikasjon.geosynkronisering.GeosynkroniseringInterface;
+import no.geonorge.skjema.standard.geosynkronisering._1_0.produkt.ChangelogIdentificationType;
+import no.geonorge.skjema.standard.geosynkronisering._1_0.produkt.ChangelogOrderType;
+import no.geonorge.skjema.standard.geosynkronisering._1_0.produkt.ChangelogStatusType.Enum;
+import no.geonorge.skjema.standard.geosynkronisering._1_0.produkt.ChangelogType;
+import no.geonorge.skjema.standard.geosynkronisering._1_0.produkt.StoredChangelogListe;
+import no.geonorge.skjema.standard.geosynkronisering._1_0.produkt.StoredChangelogType;
+
 
 
 @Controller
@@ -17,10 +24,23 @@ import no.geonorge.skjema.sosi.produktspesifikasjon.geosynkronisering.Geosynkron
 public class GeosynkroniseringController {
 	
 	GeosynkroniseringInterface geosynkroniseringImpl;
+	//default settings
+	String datasetId = "AR5";
+	String changelogId = "0";
+	String startIndex = "0";
+	String count = "1000";
+	String ResultType = "results";
+	String outputFormat = "application/gml+xml; version=3.2";
 	
 	
 	@RequestMapping(value="/start", method = RequestMethod.GET)
 	public String startUttak(ModelMap model) {
+		model.addAttribute("datasetId", datasetId);
+		model.addAttribute("changelogId", changelogId);
+		model.addAttribute("startIndex", startIndex);
+		model.addAttribute("count", count);
+		model.addAttribute("ResultType", ResultType);
+		model.addAttribute("outputFormat", outputFormat);
 		return "index";
  
 	}
@@ -28,16 +48,118 @@ public class GeosynkroniseringController {
 	@RequestMapping(value="/getLastIndex", method = RequestMethod.GET)
 	public String getLastIndex(ModelMap model) throws RemoteException, ServiceExceptionReportMessage {
 		geosynkroniseringImpl = GeosynkroniseringFactory.getGeosynkronisering();
-		BigInteger getLastIndex = geosynkroniseringImpl.GetLastIndex("3");
+		BigInteger getLastIndex = geosynkroniseringImpl.GetLastIndex(datasetId);
 		
-		model.addAttribute("lastIndex", getLastIndex);
+		model.addAttribute("datasetId", datasetId);
+		model.addAttribute("changelogId", changelogId);
+		model.addAttribute("startIndex", startIndex);
+		model.addAttribute("count", count);
+		model.addAttribute("ResultType", ResultType);
+		model.addAttribute("outputFormat", outputFormat);
+		model.addAttribute("lastIndex", "LastIndex "+ getLastIndex);
 		return "index";
  
 	}
 	@RequestMapping(value="/ListStoredChangelogs", method = RequestMethod.GET)
-	public String listStoredChangelogs(ModelMap model) {
+	public String listStoredChangelogs(ModelMap model) throws RemoteException, ServiceExceptionReportMessage {
+		geosynkroniseringImpl = GeosynkroniseringFactory.getGeosynkronisering();
+		StoredChangelogListe storedChangelogListe = geosynkroniseringImpl.ListStoredChangelogs(datasetId);
 		
-		model.addAttribute("lastIndex", 2);
+		String storedChangelogListText = "";
+		for (StoredChangelogType type : storedChangelogListe.getStoredchangelogArray()){
+			storedChangelogListText+= type.getDownloadUri() +" "+ type.getName() +" "+  type.getStartIndex() +" "+ type.getEndIndex() +" "
+					+ type.getId().getChangelogId() ;//type.getOrder()
+		}
+		
+		model.addAttribute("datasetId", datasetId);
+		model.addAttribute("changelogId", changelogId);
+		model.addAttribute("startIndex", startIndex);
+		model.addAttribute("count", count);
+		model.addAttribute("ResultType", ResultType);
+		model.addAttribute("outputFormat", outputFormat);
+		model.addAttribute("lastIndex", "storedChangelogList element 0 with end index "+ storedChangelogListe.getStoredchangelogArray(0).getName());
+		return "index";
+ 
+	}
+	@RequestMapping(value="/cancelChangelog", method = RequestMethod.GET)
+	public String cancelChangelog(ModelMap model) throws RemoteException, ServiceExceptionReportMessage {
+		geosynkroniseringImpl = GeosynkroniseringFactory.getGeosynkronisering();
+		ChangelogIdentificationType changelogId2 = ChangelogIdentificationType.Factory.newInstance();
+		BigInteger changelogIdInt = new BigInteger(changelogId);
+		changelogId2.setChangelogId(changelogIdInt);
+		geosynkroniseringImpl.CancelChangelog(changelogId2);
+		
+		model.addAttribute("datasetId", datasetId);
+		model.addAttribute("changelogId", changelogId);
+		model.addAttribute("startIndex", startIndex);
+		model.addAttribute("count", count);
+		model.addAttribute("ResultType", ResultType);
+		model.addAttribute("outputFormat", outputFormat);
+		model.addAttribute("lastIndex","Changelog " + changelogId2.getChangelogId() + " cancelled");
+		return "index";
+ 
+	}
+	
+	@RequestMapping(value="/getChangelogStatus", method = RequestMethod.GET)
+	public String getChangelogStatus(ModelMap model) throws RemoteException, ServiceExceptionReportMessage {
+		geosynkroniseringImpl = GeosynkroniseringFactory.getGeosynkronisering();
+		ChangelogIdentificationType changelogId2 = ChangelogIdentificationType.Factory.newInstance();
+		BigInteger changelogIdInt = new BigInteger(changelogId);
+		changelogId2.setChangelogId(changelogIdInt);
+		Enum getChangelogStatus = geosynkroniseringImpl.GetChangelogStatus(changelogId2);
+		
+		model.addAttribute("datasetId", datasetId);
+		model.addAttribute("changelogId", changelogId);
+		model.addAttribute("startIndex", startIndex);
+		model.addAttribute("count", count);
+		model.addAttribute("ResultType", ResultType);
+		model.addAttribute("outputFormat", outputFormat);
+		model.addAttribute("lastIndex","status " + getChangelogStatus);
+		return "index";
+ 
+	}
+	
+	@RequestMapping(value="/orderChangelog", method = RequestMethod.GET)
+	public String orderChangelog(ModelMap model) throws RemoteException, ServiceExceptionReportMessage {
+		geosynkroniseringImpl = GeosynkroniseringFactory.getGeosynkronisering();
+		ChangelogOrderType orderType = ChangelogOrderType.Factory.newInstance();
+		BigInteger startIndexInt = new BigInteger(startIndex);
+		BigInteger countInt = new BigInteger(count);
+		net.opengis.www.wfs._2_0.ResultTypeType.Enum resultTypeEnum = net.opengis.www.wfs._2_0.ResultTypeType.Enum.forString(ResultType);
+		orderType.setDatasetId(datasetId);
+		orderType.setStartIndex(startIndexInt);
+		orderType.setCount(countInt);
+		orderType.setResultType(resultTypeEnum);
+		orderType.setOutputFormat(outputFormat);
+		
+		ChangelogIdentificationType order = geosynkroniseringImpl.OrderChangelog(orderType);
+		
+		model.addAttribute("datasetId", datasetId);
+		model.addAttribute("changelogId", changelogId);
+		model.addAttribute("startIndex", startIndex);
+		model.addAttribute("count", count);
+		model.addAttribute("ResultType", ResultType);
+		model.addAttribute("outputFormat", outputFormat);
+		model.addAttribute("lastIndex","order with changelogId " + order.getChangelogId());
+		return "index";
+ 
+	}
+	
+	@RequestMapping(value="/getChangelog", method = RequestMethod.GET)
+	public String getChangelog(ModelMap model) throws RemoteException, ServiceExceptionReportMessage {
+		geosynkroniseringImpl = GeosynkroniseringFactory.getGeosynkronisering();
+		ChangelogIdentificationType changelogId2 = ChangelogIdentificationType.Factory.newInstance();
+		BigInteger changelogIdInt = new BigInteger(changelogId);
+		changelogId2.setChangelogId(changelogIdInt);
+		ChangelogType getChangelog = geosynkroniseringImpl.GetChangelog(changelogId2);
+		
+		model.addAttribute("datasetId", datasetId);
+		model.addAttribute("changelogId", changelogId);
+		model.addAttribute("startIndex", startIndex);
+		model.addAttribute("count", count);
+		model.addAttribute("ResultType", ResultType);
+		model.addAttribute("outputFormat", outputFormat);
+		model.addAttribute("lastIndex","End index "+getChangelog.getEndIndex() + "<br> DownloadURI " + getChangelog.getDownloadUri() + "<br> changelogId " + changelogId);// + getChangelog.getId().getChangelogId());
 		return "index";
  
 	}
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,10 +1,44 @@
 <html>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 <body>
 <h2>Geosynkronisering</h2>
-<input type="button" value="GetLastIndex" name="radiogroup" onClick="window.location.href='http://localhost:8081/geosync_client/getLastIndex'">
-<input type="button" value="ListStoredChangelogs" name="radiogroup" onClick="window.location.href='http://localhost:8081/geosync_client/ListStoredChangelogs'">
-<input type="button" value="GetChangelogStatus" name="radiogroup" onClick="window.location.href='http://localhost:8081/geosync_client/getChangelogStatus'">
-<br><br>
-Last index is ${lastIndex}
+<table width="600" border="0">
+<tr>
+<td><input type="button" value="GetLastIndex" name="radiogroup" onClick="window.location.href='http://localhost:8081/geosync_client/getLastIndex?datasetId='+$('#datasetId').val()"></td>
+<td><input type="button" value="OrderChangelog" name="radiogroup" onClick="window.location.href='http://localhost:8081/geosync_client/orderChangelog?datasetId='+$('#datasetId').val()+',startIndex='+$('#startIndex').val()+',count='+$('#count').val()+',ResultType='+$('#ResultType').val()+',outputFormat='+$('#outputFormat').val()"></td>
+<td><input type="button" value="ListStoredChangelogs" name="radiogroup" onClick="window.location.href='http://localhost:8081/geosync_client/ListStoredChangelogs?changelogId='+$('#changelogId').val()"></td>
+<td><input type="button" value="GetChangelogStatus" name="radiogroup" onClick="window.location.href='http://localhost:8081/geosync_client/getChangelogStatus?changelogId='+$('#changelogId').val()"></td>
+<td><input type="button" value="CancelChangelog" name="radiogroup" onClick="window.location.href='http://localhost:8081/geosync_client/cancelChangelog?changelogId='+$('#changelogId').val()"></td>
+<td><input type="button" value="GetChangelog" name="radiogroup" onClick="window.location.href='http://localhost:8081/geosync_client/getChangelog?changelogId='+$('#changelogId').val()"></td>
+</tr>
+<tr>
+<td>datasetId: <select name="datasetId" id="datasetId">
+			<option value="3"
+				selected
+				>AR5
+		</select> </td>
+</tr>
+<tr>
+<td colspan="2">changelogId: <input type="text" name="changelogId" id="changelogId" size=20 value="${changelogId}"></td>
+</tr>
+<tr>
+<td colspan="2">startIndex: <input type="text" name="startIndex" id="startIndex" size=20 value="${startIndex}"></td>
+</tr>
+<tr>
+<td colspan="2">count: <input type="text" name="count" id="count" size=20 value="${count}"></td>
+</tr>
+<tr>
+<td colspan="2">ResultType: <input type="text" name="ResultType" id="ResultType" size=20 value="${ResultType}"></td>
+</tr>
+<tr>
+<td colspan="2">outputFormat: <input type="text" name="outputFormat" id="outputFormat" size=20 value="${outputFormat}"></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<tr>
+<td>Output:</td>
+<td colspan="6">${lastIndex}</td>
+</tr>
+</table>
 </body>
 </html>
